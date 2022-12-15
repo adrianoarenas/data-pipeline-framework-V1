c516ced0830d065f7d6c8c0caa9e20d69c416249
Adriano Arenas <adrianoarenas@gmail.com>
2022-12-15 21:32:46 +0000

    updated init file

--- init_file.sh
+++ init_file.sh
@@ -16,8 +16,7 @@ cd /home/ec2-user/data-pipeline-framework-V1/
 
 echo "creating folders"
 sudo mkdir -p ./dags ./logs ./plugins
-
-sudo chown ec2-user:ec2-user ./dags
+sudo chown ec2-user:ec2-user ./dags/
 
 echo "creating env var"
 sudo echo -e "AIRFLOW_UID=$(id -u)" > .env
